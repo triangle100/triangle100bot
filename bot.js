6f1c0f76d6249e5458a57b62134c5d4994d9c547
Zane Shaw <zaneshaw.home@gmail.com>
2020-07-02 15:23:19 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ let prefix = "eea!"
 let botname = "Economee ALPHA"
 let W_message = true
 let Bot_A = "eea!help"
-let Bot_Version = "v0.2.0.17"
+let Bot_Version = "v0.2.0.18"
 let cooldown = 86400000
 let Dcoins = 500
 let wCooldown = 3600000
@@ -58,7 +58,7 @@ if (cmd === prefix + "version"){
 if (cmd === prefix + "help"){	
 	let HelpEmbed = new Discord.RichEmbed()
    .setTitle('Help Menu - Commands')
-   .setDescription("Prefix: " + prefix)
+   .setDescription("Prefix: `" + prefix + "`")
    .addField("General", "\n `eea!help` (a list of commands) \n `eea!wallet` (shows your current wealth) \n `eea!work` (work for your job) \n `eea!jobs` (a list of the jobs you can apply for) \n `eea!slots` (use the slots machine) \n `eea!daily` (claim your daily reward)", false)
    .addField("Misc", "\n `eea!version` (prints current build version) \n `eea!time` (prints current time) \n `eea!premium` (💎support Economee and get extra perks!💎)", true)
    .setColor('#f0eb62')
